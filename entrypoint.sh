aa87bedaadff764a6cc4429abc984aa2c0703bdc
Julian Psotta <22392104+MichaelsJP@users.noreply.github.com>
2021-02-02 16:31:45 +0100

    add all platforms

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,14 +17,83 @@ check_if_meta_yaml_file_exists() {
 }
 
 build_package(){
-    conda build -c conda-forge -c bioconda --output-folder . .
+    # Build for Linux
+    conda build -c conda-forge -c pytorch -c fcakyon -c districtdatalabs --output-folder . .
+
+    # Convert to other platforms
+    if [[ $INPUT_PLATFORMS == *"osx-64"* ]]; then
     conda convert -p osx-64 linux-64/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *"osx-arm64"* ]]; then
+    conda convert -p osx-64 linux-64/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *"linux-32"* ]]; then
+    conda convert -p win-64 linux-64/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *"linux-ppc64"* ]]; then
+    conda convert -p win-64 linux-64/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *"linux-ppc64le"* ]]; then
+    conda convert -p win-64 linux-64/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *"linux-s390x"* ]]; then
+    conda convert -p win-64 linux-64/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *"linux-armv6l"* ]]; then
+    conda convert -p win-64 linux-64/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *"linux-armv7l"* ]]; then
+    conda convert -p win-64 linux-64/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *"linux-aarch64"* ]]; then
+    conda convert -p win-64 linux-64/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *" win-32"* ]]; then
+    conda convert -p win-64 linux-64/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *"win-64"* ]]; then
+    conda convert -p win-64 linux-64/*.tar.bz2
+    fi
 }
 
 upload_package(){
     export ANACONDA_API_TOKEN=$INPUT_ANACONDATOKEN
     anaconda upload --label main linux-64/*.tar.bz2
+    
+    if [[ $INPUT_PLATFORMS == *"osx-64"* ]]; then
     anaconda upload --label main osx-64/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *"osx-arm64"* ]]; then
+    anaconda upload --label main osx-arm64/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *"linux-32"* ]]; then
+    anaconda upload --label main linux-32/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *"linux-ppc64"* ]]; then
+    anaconda upload --label main linux-ppc64/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *"linux-ppc64le"* ]]; then
+    anaconda upload --label main linux-ppc64le/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *"linux-s390x"* ]]; then
+    anaconda upload --label main linux-s390x/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *"linux-armv6l"* ]]; then
+    anaconda upload --label main linux-armv6l/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *"linux-armv7l"* ]]; then
+    anaconda upload --label main linux-armv7l/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *"linux-aarch64"* ]]; then
+    anaconda upload --label main linux-aarch64/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *"win-32"* ]]; then
+    anaconda upload --label main win-32/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *"win-64"* ]]; then
+    anaconda upload --label main win-64/*.tar.bz2
+    fi
+    
 }
 
 go_to_build_dir
